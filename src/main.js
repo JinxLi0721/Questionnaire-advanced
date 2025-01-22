7ff74ee169fb1c712c8d9bb2e920cd4110df3bae
Jinx <Jinx2021@gmail.com>
2025-01-22 11:40:17 +0800

    feat: add vue-router

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Antd from "ant-design-vue"
 import App from './App.vue'
+import router from "./router/index"
 
 const app = createApp(App)
 
+app.use(router)
 app.use(createPinia())
 app.use(Antd)
 
